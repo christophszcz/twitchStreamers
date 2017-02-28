2ebc6a8deb28e7fbd73603d17cae4c0836a58d40
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-28 15:22:18 -0500

    The API finally works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-	$.getJSON('', function(data){
-		console.log(JSON.stringify(data, null, 2));
-	});
 
 	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+	
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + users[0], function(data){
+		console.log(JSON.stringify(data, null, 2));
+	});
 	
 });
