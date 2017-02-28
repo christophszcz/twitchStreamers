456a842663b4f12365ee998299cc735137f01659
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-28 15:08:30 -0500

    Added jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+	$.getJSON('', function(data){
+		console.log(JSON.stringify(data, null, 2));
+	});
 
 	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 	
