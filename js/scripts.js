053c6c2ccb0223e2092bc97be6eaca58d638e3f2
christophszcz <christoph.szczechowicz@gmail.com>
2017-03-01 02:05:46 -0500

    There is now a true seperation of online and offline channels.

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,11 @@ $(document).ready(function(){
 		$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + users[i], function(data){
 			//console.log(JSON.stringify(data, null, 2));
 
-			// var html = "";
-
 			if (data.stream === null ){
 				// html += "<li>" +  data.stream + "</li>";
 				// $('.test').html(html);
-				$.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + users[i], function(json){
-					offline.push(json.display_name);
-				});
+				var result = data._links.self.replace('https://api.twitch.tv/kraken/streams/', '');
+				offline.push(result);
 			} else if (data.stream !== null) {
 				online.push(data.stream.channel.display_name);
 				// html += "<li>" +  data.stream.channel.display_name + "</li>";
@@ -26,15 +23,5 @@ $(document).ready(function(){
 
 	console.log(online);
 	console.log(offline);
-
-	// $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + users[0], function(data){
-	// 		console.log(JSON.stringify(data, null, 2));
-	// 		// if (data.stream !== null ){
-	// 			var info = "<p>" +  data.stream.channel.display_name + "</p>";
-	// 			$('.test').html(info);
-	// 		// }
-
-	// });
 	 
-	
 });
