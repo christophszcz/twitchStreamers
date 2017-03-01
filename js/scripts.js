5a75ba90f3b8d530a54f0dadb60ea7ca630a592a
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-28 23:59:39 -0500

    Seperated the online and offline channels.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,40 @@
 $(document).ready(function(){
 
 	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-	
-	$.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + users[0], function(data){
-		console.log(JSON.stringify(data, null, 2));
-	});
+	var online = [];
+	var offline = []
+
+	for (var i = 0; i < users.length ; i++) {
+		$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + users[i], function(data){
+			//console.log(JSON.stringify(data, null, 2));
+
+			// var html = "";
+
+			if (data.stream === null ){
+				// html += "<li>" +  data.stream + "</li>";
+				// $('.test').html(html);
+				$.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + users[i], function(json){
+					offline.push(json.display_name);
+				});
+			} else if (data.stream !== null) {
+				online.push(data.stream.channel.display_name);
+				// html += "<li>" +  data.stream.channel.display_name + "</li>";
+				// $('.test').html(html);
+			}
+		});
+	}
+
+	console.log(online);
+	console.log(offline);
+
+	// $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + users[0], function(data){
+	// 		console.log(JSON.stringify(data, null, 2));
+	// 		// if (data.stream !== null ){
+	// 			var info = "<p>" +  data.stream.channel.display_name + "</p>";
+	// 			$('.test').html(info);
+	// 		// }
+
+	// });
+	 
 	
 });
