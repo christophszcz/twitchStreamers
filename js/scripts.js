1c4a6e08e83698a87681c90740f447d58889434e
christophszcz <christoph.szczechowicz@gmail.com>
2017-03-03 23:02:50 -0500

    There is a seperation between online and offline channels.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,24 @@
 $(document).ready(function(){
 
 	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-	var online = [];
-	var offline = []
 
 	for (var i = 0; i < users.length ; i++) {
 		$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + users[i], function(data){
 			//console.log(JSON.stringify(data, null, 2));
 
 			if (data.stream === null ){
-				// html += "<li>" +  data.stream + "</li>";
-				// $('.test').html(html);
-				var result = data._links.self.replace('https://api.twitch.tv/kraken/streams/', '');
-				offline.push(result);
+				$('.offline').show();
+				var offlineResult = data._links.self.replace('https://api.twitch.tv/kraken/streams/', '');
+				// console.log(result);
+				$('.offline').append(offlineResult + " ");
+				//offline.push(result);
 			} else if (data.stream !== null) {
-				online.push(data.stream.channel.display_name);
+				$('.online').show();
+				var online =  data.stream.channel.display_name;
 				// html += "<li>" +  data.stream.channel.display_name + "</li>";
-				// $('.test').html(html);
+				$('.online').append(online + " ");
 			}
 		});
 	}
-
-	console.log(online);
-	console.log(offline);
-	 
+ 
 });
