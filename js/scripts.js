66c4fd127122a01f17ec0a8652e66597a2318283
christophszcz <christoph.szczechowicz@gmail.com>
2017-03-18 13:31:46 -0400

    Remove the border box CSS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,41 @@
 $(document).ready(function(){
 
 	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+	var array = [];
 
 	for (var i = 0; i < users.length ; i++) {
 		$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + users[i], function(data){
 			//console.log(JSON.stringify(data, null, 2));
 
-			$('.online-button').click(function(){
-				$('.offline').hide();
-				if (data.stream !== null) {
-					$('.online').show();
-					var online =  data.stream.channel.display_name;
-					// html += "<li>" +  data.stream.channel.display_name + "</li>";
-					$('.online').append(online + " ");
-				}
-			});
 
-			$('.offline-button').click(function(){
-				$('.online').hide();
-				if (data.stream === null ){
-					$('.offline').show();
-					var offlineResult = data._links.self.replace('https://api.twitch.tv/kraken/streams/', '');
-					// console.log(result);
-					$('.offline').append(offlineResult + " ");
-					//offline.push(result);
-				}
-			});
+			// $('.online-button').click(function(){
+			// 	$('.offline').hide();
+			// 	if (data.stream !== null) {
+			// 		$('.online').show();
+			// 		var online =  data.stream.channel.display_name;
+			// 		// html += "<li>" +  data.stream.channel.display_name + "</li>";
+			// 		$('.online').append(online + " ");
+			// 	}
+			// });
+
+			// $('.offline-button').click(function(){
+			// 	$('.online').hide();
+			// 	if (data.stream === null ){
+			// 		$('.offline').show();
+			// 		var offlineResult = data._links.self.replace('https://api.twitch.tv/kraken/streams/', '');
+			// 		// console.log(result);
+			// 		$('.offline').append(offlineResult + " ");
+			// 		//offline.push(result);
+			// 	}
+			// });
+
+			// $('.all-button').click(function(){
+			// 	if( $('.online').css('display') === 'none' ){
+			// 		$('.online').show();
+			// 	} else if ($('.offline').css('display') === 'none'){
+			// 		$('.offline').show();
+			// 	}
+			// });
 
 		});
 	}
