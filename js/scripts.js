f4e4a0c214e308fe03f07d9fcd77017107fd26b9
christophszcz <christoph.szczechowicz@gmail.com>
2017-03-26 23:36:00 -0400

    Make it not allowed for more values to show up the higher amount of times that you press the Online button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,39 @@
 var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
- 
-function loadOnlineXMLDoc() {
-	document.getElementById('offline-id').style.display = 'none';
-	document.getElementById('online-id').style.display = 'block';
-  for (var i = 0; i < users.length ; i++) { 
-    var xmlhttp = new XMLHttpRequest();
-    var url = "https://wind-bow.glitch.me/twitch-api/streams/" + users[i];
-    
-      (function(xmlhttp){
-        xmlhttp.onreadystatechange = function() {
-	        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-	          if (xmlhttp.status == 200) {
-	          	var data = xmlhttp.responseText;
-							var jsonResponse  = JSON.parse(data);
-							document.getElementById('title').innerHTML = "<h3>Online</h3>"
-	            document.getElementById('online-id').innerHTML += (jsonResponse['stream']['channel']['display_name'] + "<br/><br/>");
-	          }else if (xmlhttp.status == 400) {
-	            console.log('There was an error 400');
-	          }else {  
-	            console.log('Something else other than 200 was returned.');
-	          }
-	        }
-      	}
-      })(xmlhttp)
+var onlineCounter = 0;
 
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send(); 
-  }
+function loadOnlineXMLDoc() {
+	if (onlineCounter === 0){
+		onlineCounter ++;
+	  for (var i = 0; i < users.length ; i++) { 
+	    var xmlhttp = new XMLHttpRequest();
+	    var url = "https://wind-bow.glitch.me/twitch-api/streams/" + users[i];
+	    
+	      (function(xmlhttp){
+	        xmlhttp.onreadystatechange = function() {
+		        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+		          if (xmlhttp.status == 200) {
+		          	var data = xmlhttp.responseText;
+								var jsonResponse  = JSON.parse(data);
+								document.getElementById('title').innerHTML = "<h3>Online</h3>"
+		            document.getElementById('online-id').innerHTML += (jsonResponse['stream']['channel']['display_name'] + "<br/><br/>");
+		          }else if (xmlhttp.status == 400) {
+		            console.log('There was an error 400');
+		          }else {  
+		            console.log('Something else other than 200 was returned.');
+		          }
+		        }
+	      	}
+	      })(xmlhttp)
+
+	    xmlhttp.open("GET", url, true);
+	    xmlhttp.send(); 
+	  }
+	}else{
+		document.getElementById('offline-id').style.display = 'none';
+		document.getElementById('online-id').style.display = 'block';
+	}
 }
+
 var onlineButton = document.getElementById('online-button-id');
 onlineButton.addEventListener('click', loadOnlineXMLDoc);
 
