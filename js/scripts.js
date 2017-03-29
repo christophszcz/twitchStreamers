0baad939467392c0af1b206f15e13ccb683f5b7e
christophszcz <christoph.szczechowicz@gmail.com>
2017-03-28 23:59:13 -0400

    Add links to Offline & Online  channels.

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ function loadOnlineXMLDoc() {
 								document.getElementById('title').innerHTML = "<h3>Online</h3>";
 
 		          	if(jsonResponse['stream']!= null){
-			            document.getElementById('online-id').innerHTML += (jsonResponse['stream']['channel']['display_name'] + "<br/><br/>");
+			            document.getElementById('online-id').innerHTML += ("<a href='" + jsonResponse['stream']['channel']['url'] + "' target='_blank'>" + jsonResponse['stream']['channel']['display_name'] + "</a>" + "<br/><br/>");
 		          	} 
 		          }else if (xmlhttp.status == 400) {
 		            console.log('There was an error 400');
@@ -68,7 +68,7 @@ function loadOfflineXMLDoc() {
 								document.getElementById('title').innerHTML = "<h3>Offline</h3>";
 								if (jsonResponse['stream'] === null){
 									var title = jsonResponse['_links']['channel'].replace('https://api.twitch.tv/kraken/channels/', '');
-									document.getElementById('offline-id').innerHTML += (title + "<br/><br/>");
+									document.getElementById('offline-id').innerHTML += ("<a href='https://www.twitch.tv/" + title  + "' target='_blank'>" + title + "</a>" + "<br/><br/>");
 								}
 		          }else if (xmlhttp.status == 400) {
 		            console.log('There was an error 400');
